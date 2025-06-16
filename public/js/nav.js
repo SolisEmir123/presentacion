23f1855765725ab67822295fcbd6b6e8502783a2
CappuccinoScript <darioandrenavarro@gmail.com>
2025-06-15 23:00:24 -0600

    Nuevo código JS 1.01

--- public/js/nav.js
+++ public/js/nav.js
@@ -2,6 +2,7 @@ function toggleMenu() {
     const sidebar = document.getElementById('sidebar');
     const menuButton = document.getElementById('menuButton');
 
+    menuButton.style.transition = "transform 0.3s ease"
     sidebar.classList.toggle('open');
 
     if(sidebar.classList.contains('open')){
